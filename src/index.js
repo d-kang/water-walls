2b424fce2d7688f191fa78546ad91e76094f2c54
d-kang <david.kang714@gmail.com>
2018-01-26 21:30:27 -0800

    Can Dynamically Create a 2D Matrix of Solid Blocks and Black Walls

--- src/index.js
+++ src/index.js
@@ -17,26 +17,53 @@ const arrayToMatrix = (arr) => {
   const row = Math.max(...arr);
   const col = arr.length;
   const matrix = createMatrix(row, col);
+
+  for (let i = 0; i < row; i++) {
+    for (let k = 0; k < col; k++) {
+      if (col - i - 2 < arr[k]) {
+        matrix[i][k] = 1;
+      }
+    }
+  }
+
+  const waterWalls = [3, 8, 11]
+
+  for (let i = 0; i < row; i++) {
+    for (let k = 0; k < col; k++) {
+      if (k === waterWalls[0] - 1 && matrix[i][k] === 1) {
+        matrix[i][k] = 3;
+      }
+      if (k === waterWalls[1] - 1 && matrix[i][k] === 1) {
+        matrix[i][k] = 3;
+      }
+    }
+  }
+
+
+  return matrix;
 };
 
-arrayToMatrix([5, 3, 7, 2, 6, 4, 5, 9, 1, 2]);
 
+
+
+const result = arrayToMatrix([5, 3, 7, 2, 6, 4, 5, 9, 1, 2]);
+console.log('result', result);
 const waterWalls = [
-  [0, 0, 0, 0, 0, 0, 0, 2, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 2, 0, 0],
-  [0, 0, 2, 3, 3, 3, 3, 2, 0, 0],
-  [0, 0, 2, 3, 1, 3, 3, 2, 0, 0],
-  [1, 3, 2, 3, 1, 3, 1, 2, 0, 0],
-  [1, 3, 2, 3, 1, 1, 1, 2, 0, 0],
-  [1, 1, 2, 3, 1, 1, 1, 2, 0, 0],
-  [1, 1, 2, 1, 1, 1, 1, 2, 3, 1],
-  [1, 1, 2, 1, 1, 1, 1, 2, 1, 1],
+  [0, 0, 0, 0, 0, 0, 0, 2, 0, 0], // 9
+  [0, 0, 0, 0, 0, 0, 0, 2, 0, 0], // 8
+  [0, 0, 2, 3, 3, 3, 3, 2, 0, 0], // 7
+  [0, 0, 2, 3, 1, 3, 3, 2, 0, 0], // 6
+  [1, 3, 2, 3, 1, 3, 1, 2, 0, 0], // 5
+  [1, 3, 2, 3, 1, 1, 1, 2, 0, 0], // 4
+  [1, 1, 2, 3, 1, 1, 1, 2, 0, 0], // 3
+  [1, 1, 2, 1, 1, 1, 1, 2, 3, 1], // 2
+  [1, 1, 2, 1, 1, 1, 1, 2, 1, 1], // 1
 ];
 
 (function(matrix) {
-  const rows = matrix.length;
+  const row = matrix.length;
   const col = matrix[0].length;
-  for (let i = 0; i < rows; i++) {
+  for (let i = 0; i < row; i++) {
     const divRow = document.createElement('div');
     divRow.setAttribute('class', 'row');
     for (let k = 0; k < col + 1; k++) {
